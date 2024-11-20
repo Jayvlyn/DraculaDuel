12b333fd92d2061008e8970b3a49f4938eadb5f2
Snake-EyE-12 <chase.guymon@q.com>
2024-11-19 18:09:55 -0700

    Perception Classes Creation

---
+++ Assets/Scripts/AIPerception.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPerception : MonoBehaviour
+{
+    [SerializeField] private PerceptionShape _perceptionShape;
+    public List<TargetHitData> PerceiveTargets(Vector3 forwardDirection)
+    {
+        return null;
+    }
+}
+
+
+[System.Serializable]
+public class PerceptionShape
+{
+     
+}
+
+
+
+
+public class TargetHitData
+{
+    
+}
