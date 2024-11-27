df907497ebf0e2ba2b9111ba2f4ca06c042ad9ae
bethompson04 <benniert06@gmail.com>
2024-11-26 17:05:20 -0700

    Garlic Grenade

--- Assets/Bennie/Scripts/StinkyGarlicPlume.cs
+++ Assets/Bennie/Scripts/StinkyGarlicPlume.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class StinkyGarlicPlume : MonoBehaviour
 {
-    [SerializeField] public float GasDuration = 2;
+    [SerializeField] public float GasDuration = 5;
     [SerializeField] public Vector3 GasDirection;
     [SerializeField] public float GasSpreadSpeed;
+    [SerializeField] public Material GasMaterial;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Renderer m_renderer;
+
     void Start()
     {
+        GasSpreadSpeed = GasSpreadSpeed * 0.001f;
+        m_renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,14 @@ public class StinkyGarlicPlume : MonoBehaviour
         GasDuration -= Time.deltaTime;
 
         this.transform.position += GasDirection * GasSpreadSpeed;
+
+
+        Color color = m_renderer.material.color;
+        if(color.a > 0.01f)
+        { 
+            color.a -= 0.00015f;
+            m_renderer.material.color = color;
+        }
 
     }
 }
--- Assets/Scripts/Weapon/GarlicGasGrenade.cs
+++ Assets/Scripts/Weapon/GarlicGasGrenade.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 public class GarlicGasGrenade : WeaponProjectile
 {
 
+    [SerializeField] public GameObject plumeObject;
+    public float plumeSpawnDelay = 1.0f;
+    public bool Active = false;
+    public float GrenadeLifeTime = 10.0f;
     
-    public float plumeSpawnDelay;
-    
+    private float plumeSpawnTime;
     void Start()
     {
-        
+        Active = false;
+        plumeSpawnTime = plumeSpawnDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rb.linearVelocity == Vector3.zero)
+        if (Active)
         {
+            GrenadeLifeTime -= Time.deltaTime;
+            plumeSpawnTime -= Time.deltaTime;
 
+            if (plumeSpawnTime <= 0)
+            {
+                // new plumeObject
+                float xVel = UnityEngine.Random.Range(-1.0f, 1.0f);
+                float zVel = UnityEngine.Random.Range(-1.0f, 1.0f);
+                float yVel = UnityEngine.Random.Range(0.0f, 1.0f);
+
+                GameObject newPlume = Instantiate(plumeObject, this.transform.position, this.transform.rotation);
+                newPlume.GetComponent<StinkyGarlicPlume>().GasDirection = new Vector3(xVel, yVel, zVel);
+                plumeSpawnTime = plumeSpawnDelay;
+            }
         }
+        if(GrenadeLifeTime <= 0.0f)
+        {
+            Debug.Log("Kill");
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            Active = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
     }
 }
